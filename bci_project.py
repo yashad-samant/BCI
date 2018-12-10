6c61c6144a54292659b07e7c37c115d1db367c23
Yashad Samant <yashadsamant@Yashads-MacBook-Pro.local>
2018-12-09 16:43:16 -0800

    standardization and separation achieved

--- bci_project.py
+++ bci_project.py
@@ -9,14 +9,52 @@ Created on Thu Nov  1 20:32:06 2018
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-import sklearn as sk
+from sklearn import preprocessing
 import vrep
 import sys
 
 class BrainComputerInterface():
+    def __init__(self, dataName):
+        self.data = self.extractData(dataName)
+    
+    def extractData(self, dataName):
+        """
+        Input: data.csv
+        Output: dataframe
+        """
+        col = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+               16,17,18,19,20,21,25]
+        convert = lambda x: 1 if int(x)==769 else 2 if int(x)==770 else 3 if int(x)==771 else 4 
+        data = pd.read_csv(dataName, usecols=col, converters={25:convert})
+        data = data.values
+        return data
+    
     def dataPreProcessing(self):
+        """
+        input: data(ndarray)
+        out: Xtrain,Ytrain,Xtest,Ytest,Xval,Yval
+        """
         
-        return None
+        data = self.data
+        X = data[:,0:-1]
+        
+        X = preprocessing.scale(X)
+        Y = data[:,-1]
+        
+        #Randomize dataset
+        randi = np.arange(len(X))
+        np.random.shuffle(randi)
+        X = X[randi]
+        print(X.shape)
+        Y = Y[randi]
+        print(Y.shape)
+        
+        # Separate data in train,validate and test
+        Xtrain = X[0:int(0.6*len(X)),:]; Ytrain = Y[0:int(0.6*len(X))]  
+        Xval = X[int(0.6*len(X)):int(0.8*len(X)),:]; Yval = Y[int(0.6*len(X)):int(0.8*len(X))]
+        Xtest = X[int(0.8*len(X)):,:]; Ytest = Y[int(0.8*len(X)):]
+        
+        return X,Y,Xtrain,Ytrain,Xtest,Ytest,Xval,Yval
     
     def setConnection(self):
         vrep.simxFinish(-1) # just in case, close all opened connections
@@ -39,10 +77,10 @@ class BrainComputerInterface():
 
         
 if __name__ == "__main__":        
-    BCI = BrainComputerInterface()
-    BCI.setConnection() 
-    while True:
-        BCI.createHandle()     
+    BCI = BrainComputerInterface('data.csv')
+    X,Y,Xtrain,Ytrain,Xtest,Ytest,Xval,Yval = BCI.dataPreProcessing()
+
+    
         
         
 
